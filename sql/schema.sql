f5d732d43389fa42e694510589ce25c91af79be5
hadeel alanazi <hadeelalanazi.2025@gmail.com>
2025-08-02 10:50:09 +0300

    Add files via upload

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,1 +1,29 @@
+CREATE DATABASE IF NOT EXISTS smart_finance;
+USE smart_finance;
 
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(100),
+  email VARCHAR(100) UNIQUE,
+  password VARCHAR(255),
+  income DECIMAL(10,2)
+);
+
+CREATE TABLE transactions (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  user_id INT,
+  category VARCHAR(50),
+  amount DECIMAL(10,2),
+  date DATE,
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE goals (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  user_id INT,
+  title VARCHAR(100),
+  target_amount DECIMAL(10,2),
+  current_saved DECIMAL(10,2),
+  deadline DATE,
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
